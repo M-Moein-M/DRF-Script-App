f1d3d56045adf7cafbec6a5ad1828d65e4bc35e7
moein <mohammadmoein.moradi@gmail.com>
2021-08-14 10:33:02 +0430

    add scripts app

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('snippets.urls')),
+    path('', include('scripts.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
