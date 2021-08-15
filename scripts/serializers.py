a3994e714991121ecb6429ce4b511ca3895d087b
moein <mohammadmoein.moradi@gmail.com>
2021-08-15 13:23:00 +0430

    add Script serializer

---
+++ scripts/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from snippets.models import Script
+
+
+class ScriptSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    owner = serializers.ReadOnlyField(source='owner.username')
+    snippets = serializers.CharField(max_length=1023, default='')
+    id = serializers.IntegerField(required=False)
+
+    def create(self, validated_data):
+        instance = Script.objects.create(**validated_data)
+        return instance
