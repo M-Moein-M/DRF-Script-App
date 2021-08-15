77466751729be13636c6e55a4d329116fa0f02f4
moein <mohammadmoein.moradi@gmail.com>
2021-08-15 13:26:18 +0430

    add ScriptList view

--- scripts/views.py
+++ scripts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from scripts.serializers import ScriptSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class ScriptList(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        serializer = ScriptSerializer(data=request.data)
+        if serializer.is_valid():
+            instance = serializer.save(owner=request.user)
+            return Response(data=ScriptSerializer(instance).data,
+                            status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
