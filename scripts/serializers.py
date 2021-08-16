d8d5f33f89f3575b02eb2b78f1c070f2021a64d9
moein <mohammadmoein.moradi@gmail.com>
2021-08-16 08:23:18 +0430

    change ScriptSerializer id field to read-only

--- scripts/serializers.py
+++ scripts/serializers.py
@@ -6,7 +6,7 @@ class ScriptSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     owner = serializers.ReadOnlyField(source='owner.username')
     snippets = serializers.CharField(max_length=1023, default='')
-    id = serializers.IntegerField(required=False)
+    id = serializers.ReadOnlyField(required=False)
 
     def create(self, validated_data):
         instance = Script.objects.create(**validated_data)
