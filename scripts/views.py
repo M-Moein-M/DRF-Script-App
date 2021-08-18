bd3f78bb0d8a86431b4d7475ae8b055d88726882
moein <mohammadmoein.moradi@gmail.com>
2021-08-18 08:43:32 +0430

    refactor ScriptList view

--- scripts/views.py
+++ scripts/views.py
@@ -1,23 +1,14 @@
 from rest_framework import generics
-from rest_framework import mixins
 from scripts.serializers import ScriptSerializer
 from snippets.models import Script
 
 
-class ScriptList(mixins.ListModelMixin,
-                 mixins.CreateModelMixin,
-                 generics.GenericAPIView):
+class ScriptList(generics.ListCreateAPIView):
     """Listing and creating script objects"""
 
     queryset = Script.objects.all().order_by('id')
     serializer_class = ScriptSerializer
     pagination_class = None
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
     def perform_create(self, serializer, *args, **kwargs):
         serializer.save(owner=self.request.user)
