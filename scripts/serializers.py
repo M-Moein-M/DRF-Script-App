0a325b3eef4731b61a3dae3405b5d08e271e441b
moein <mohammadmoein.moradi@gmail.com>
2021-08-29 10:24:41 +0430

    refactor ScriptSerializer using ModelSerializer

--- scripts/serializers.py
+++ scripts/serializers.py
@@ -2,40 +2,30 @@ from rest_framework import serializers
 from snippets.models import Script, Snippet
 
 
-def validate_snippets_field(value):
-    """snippets field must be a comma separated string of existing snippet id"""
-    try:
-        id_set = {int(s_id.strip())
-                  for s_id in value.split(',') if s_id.strip()}
-        existing_ids = set(Snippet.objects.values_list('id', flat=True))
-        common_ids = id_set & existing_ids
-        if common_ids == id_set:
-            return
-        else:
-            msg = 'Invalid snippets id.'
-            raise serializers.ValidationError(msg)
-    except ValueError:
-        msg = 'Cannot convert id to int.'
-        raise serializers.ValidationError(msg)
-
-
-class ScriptSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=255)
+class ScriptSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    snippets = serializers.CharField(max_length=1023,
-                                     default='',
-                                     validators=[validate_snippets_field])
-    id = serializers.ReadOnlyField(required=False)
 
-    def create(self, validated_data):
-        instance = Script.objects.create(**validated_data)
-        return instance
+    class Meta:
+        model = Script
+        fields = '__all__'
+        read_only_fields = ['id', 'owner']
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.snippets = validated_data.get('snippets', instance.snippets)
-        instance.save()
-        return instance
+    def validate_snippets(self, value):
+        """snippets field must be
+        a comma separated string of existing snippet id"""
+        try:
+            id_set = {int(s_id.strip())
+                      for s_id in value.split(',') if s_id.strip()}
+            existing_ids = set(Snippet.objects.values_list('id', flat=True))
+            common_ids = id_set & existing_ids
+            if common_ids == id_set:
+                return value
+            else:
+                msg = 'Invalid snippets id.'
+                raise serializers.ValidationError(msg)
+        except ValueError:
+            msg = 'Cannot convert id to int.'
+            raise serializers.ValidationError(msg)
 
 
 class ScriptDetailSerializer(ScriptSerializer):
